1cc0f206a3809226d6ff910391c7572b8a30160b
Oscar Ayala <00068422@uca.edu.sv>
2025-03-01 10:06:28 -0600

    fixing render problem

--- index.js
+++ index.js
@@ -34,16 +34,18 @@ const roomLocationMap = {
 };
 
 const getLocationUrl = (roomName) => {
-    const imagePath = roomLocationMap[roomName];
-    if (!imagePath) return ""; 
+  const imagePath = roomLocationMap[roomName];
+  if (!imagePath) return ""; 
+
+  const baseURL = process.env.NODE_ENV === "production"
+    ? "https://carteleraback.onrender.com"
+    : "http://localhost:3001";
+
+  return `${baseURL}/assets/images/${path.basename(imagePath)}`;
+};
+
   
-    const baseURL = process.env.NODE_ENV === "production"
-      ? "https://carteleraback.onrender.com/"
-      : "http://localhost:3001";
-  
-    return `${baseURL}/assets/images/${path.basename(imagePath)}`;
-  };
-  
+
 const generateId = () => {
     const id = Math.floor(Math.random()*1000)
     return id
@@ -85,13 +87,9 @@ app.get("/api/meetings", (req, res) => {
   });
   
   // Servir React después de las rutas de la API
+  app.use(express.static(path.join(__dirname, "dist")));
 
-  app.use('/assets/images', (req, res, next) => {
-    console.log(`Solicitud de imagen: ${req.url}`);
-    console.log(`Ruta completa: ${path.join(__dirname, 'assets', 'images', req.url)}`);
-    next();
-  }, express.static(path.join(__dirname, 'assets', 'images')));
-
+  app.use('/assets/images', express.static(path.join(__dirname, 'assets', 'images')));
   
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "dist", "index.html"));
